0fd54eeef1824b6ca62c6d9180fe5d2bee83ae52
Steven Vachon <contact@svachon.com>
2020-03-03 12:34:22 -0500

    Minor changes

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,4 @@
+'use strict';
 module.exports = {
   ...require('@grafana/toolkit/src/config/prettier.plugin.config.json'),
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+'use strict';
 const PerspectivePlugin = require('@finos/perspective-webpack-plugin');
 
 module.exports.getWebpackConfig = (config, options) => ({
