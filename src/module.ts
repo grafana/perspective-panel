ee21e029ffa5070dede25615d310897bc2837043
Ryan McKinley <ryantxu@gmail.com>
2020-06-25 15:24:51 -0700

    update libs

--- src/module.ts
+++ src/module.ts
@@ -3,4 +3,6 @@ import { PanelPlugin } from '@grafana/data';
 import { PerspectivePanel } from './PerspectivePanel';
 import { PanelEditor } from 'PanelEditor';
 
-export const plugin = new PanelPlugin<PerspectiveOptions>(PerspectivePanel).setEditor(PanelEditor).setDefaults(defaults);
+export const plugin = new PanelPlugin<PerspectiveOptions>(PerspectivePanel)
+  .setEditor(PanelEditor)
+  .setDefaults(defaults);
